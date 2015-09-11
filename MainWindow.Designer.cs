8a1ace50b3453eff199feb0eb24d765b916b4049
Becker Johannes <johannes_becker@live.de>
2015-09-11 09:30:08 +0200

    some Validations

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -38,11 +38,21 @@
             this.txtOutput = new System.Windows.Forms.TextBox();
             this.pgProgress = new System.Windows.Forms.ProgressBar();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVarExp = new System.Windows.Forms.TextBox();
+            this.BtnLoadVarExp = new System.Windows.Forms.Button();
+            this.LblRes = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtExtColLogs = new System.Windows.Forms.TextBox();
+            this.txtExtColTrends = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // txtSourceFolder
             // 
-            this.txtSourceFolder.Location = new System.Drawing.Point(124, 12);
+            this.txtSourceFolder.Location = new System.Drawing.Point(124, 44);
             this.txtSourceFolder.Name = "txtSourceFolder";
             this.txtSourceFolder.Size = new System.Drawing.Size(283, 20);
             this.txtSourceFolder.TabIndex = 0;
@@ -50,7 +60,7 @@
             // lblSourceFolder
             // 
             this.lblSourceFolder.AutoSize = true;
-            this.lblSourceFolder.Location = new System.Drawing.Point(11, 15);
+            this.lblSourceFolder.Location = new System.Drawing.Point(11, 47);
             this.lblSourceFolder.Name = "lblSourceFolder";
             this.lblSourceFolder.Size = new System.Drawing.Size(73, 13);
             this.lblSourceFolder.TabIndex = 1;
@@ -58,7 +68,7 @@
             // 
             // btnSelectSourceFolder
             // 
-            this.btnSelectSourceFolder.Location = new System.Drawing.Point(413, 10);
+            this.btnSelectSourceFolder.Location = new System.Drawing.Point(413, 42);
             this.btnSelectSourceFolder.Name = "btnSelectSourceFolder";
             this.btnSelectSourceFolder.Size = new System.Drawing.Size(27, 23);
             this.btnSelectSourceFolder.TabIndex = 2;
@@ -68,7 +78,7 @@
             // 
             // btnSelectTargetFolder
             // 
-            this.btnSelectTargetFolder.Location = new System.Drawing.Point(413, 36);
+            this.btnSelectTargetFolder.Location = new System.Drawing.Point(413, 68);
             this.btnSelectTargetFolder.Name = "btnSelectTargetFolder";
             this.btnSelectTargetFolder.Size = new System.Drawing.Size(27, 23);
             this.btnSelectTargetFolder.TabIndex = 5;
@@ -79,7 +89,7 @@
             // lblTargetFolder
             // 
             this.lblTargetFolder.AutoSize = true;
-            this.lblTargetFolder.Location = new System.Drawing.Point(11, 41);
+            this.lblTargetFolder.Location = new System.Drawing.Point(11, 73);
             this.lblTargetFolder.Name = "lblTargetFolder";
             this.lblTargetFolder.Size = new System.Drawing.Size(70, 13);
             this.lblTargetFolder.TabIndex = 4;
@@ -87,14 +97,14 @@
             // 
             // txtTargetFolder
             // 
-            this.txtTargetFolder.Location = new System.Drawing.Point(124, 38);
+            this.txtTargetFolder.Location = new System.Drawing.Point(124, 70);
             this.txtTargetFolder.Name = "txtTargetFolder";
             this.txtTargetFolder.Size = new System.Drawing.Size(283, 20);
             this.txtTargetFolder.TabIndex = 3;
             // 
             // btnConvert
             // 
-            this.btnConvert.Location = new System.Drawing.Point(446, 35);
+            this.btnConvert.Location = new System.Drawing.Point(446, 67);
             this.btnConvert.Name = "btnConvert";
             this.btnConvert.Size = new System.Drawing.Size(69, 23);
             this.btnConvert.TabIndex = 6;
@@ -107,11 +117,11 @@
             this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtOutput.Location = new System.Drawing.Point(12, 69);
+            this.txtOutput.Location = new System.Drawing.Point(12, 101);
             this.txtOutput.Multiline = true;
             this.txtOutput.Name = "txtOutput";
             this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtOutput.Size = new System.Drawing.Size(602, 339);
+            this.txtOutput.Size = new System.Drawing.Size(690, 307);
             this.txtOutput.TabIndex = 7;
             // 
             // pgProgress
@@ -120,12 +130,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pgProgress.Location = new System.Drawing.Point(12, 413);
             this.pgProgress.Name = "pgProgress";
-            this.pgProgress.Size = new System.Drawing.Size(602, 23);
+            this.pgProgress.Size = new System.Drawing.Size(690, 23);
             this.pgProgress.TabIndex = 8;
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(521, 35);
+            this.btnCancel.Location = new System.Drawing.Point(521, 67);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(69, 23);
             this.btnCancel.TabIndex = 6;
@@ -133,11 +143,109 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(413, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Varexp.dat:";
+            // 
+            // txtVarExp
+            // 
+            this.txtVarExp.Location = new System.Drawing.Point(124, 15);
+            this.txtVarExp.Name = "txtVarExp";
+            this.txtVarExp.Size = new System.Drawing.Size(283, 20);
+            this.txtVarExp.TabIndex = 9;
+            // 
+            // BtnLoadVarExp
+            // 
+            this.BtnLoadVarExp.Location = new System.Drawing.Point(446, 13);
+            this.BtnLoadVarExp.Name = "BtnLoadVarExp";
+            this.BtnLoadVarExp.Size = new System.Drawing.Size(69, 23);
+            this.BtnLoadVarExp.TabIndex = 12;
+            this.BtnLoadVarExp.Text = "Load";
+            this.BtnLoadVarExp.UseVisualStyleBackColor = true;
+            this.BtnLoadVarExp.Click += new System.EventHandler(this.BtnLoadVarExp_Click);
+            // 
+            // LblRes
+            // 
+            this.LblRes.AutoSize = true;
+            this.LblRes.Location = new System.Drawing.Point(537, 18);
+            this.LblRes.Name = "LblRes";
+            this.LblRes.Size = new System.Drawing.Size(29, 13);
+            this.LblRes.TabIndex = 13;
+            this.LblRes.Text = "RES";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(607, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Extended Columns";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(607, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Logs";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(607, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Trends";
+            // 
+            // txtExtColLogs
+            // 
+            this.txtExtColLogs.Location = new System.Drawing.Point(654, 39);
+            this.txtExtColLogs.Name = "txtExtColLogs";
+            this.txtExtColLogs.Size = new System.Drawing.Size(45, 20);
+            this.txtExtColLogs.TabIndex = 17;
+            this.txtExtColLogs.Text = "0";
+            // 
+            // txtExtColTrends
+            // 
+            this.txtExtColTrends.Location = new System.Drawing.Point(654, 64);
+            this.txtExtColTrends.Name = "txtExtColTrends";
+            this.txtExtColTrends.Size = new System.Drawing.Size(45, 20);
+            this.txtExtColTrends.TabIndex = 18;
+            this.txtExtColTrends.Text = "0";
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(626, 441);
+            this.ClientSize = new System.Drawing.Size(714, 441);
+            this.Controls.Add(this.txtExtColTrends);
+            this.Controls.Add(this.txtExtColLogs);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LblRes);
+            this.Controls.Add(this.BtnLoadVarExp);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtVarExp);
             this.Controls.Add(this.pgProgress);
             this.Controls.Add(this.txtOutput);
             this.Controls.Add(this.btnCancel);
@@ -167,6 +275,16 @@
         private System.Windows.Forms.TextBox txtOutput;
         private System.Windows.Forms.ProgressBar pgProgress;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtVarExp;
+        private System.Windows.Forms.Button BtnLoadVarExp;
+        private System.Windows.Forms.Label LblRes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtExtColLogs;
+        private System.Windows.Forms.TextBox txtExtColTrends;
     }
 }
 
